c0cb06439fd99c18e50a3ff848d00d48879500a8
hajareshyam <hajareshyam@gmail.com>
2019-03-02 11:37:02 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+
+// Push notification plugins
+import { FCM } from "@ionic-native/fcm/ngx";
+
+import { AppAvailability } from '@ionic-native/app-availability/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +22,9 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    AppAvailability,
+    FCM,
+    SocialSharing,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { SocialSharing } from "@ionic-native/social-sharing/ngx";
+import { Platform } from "@ionic/angular";
+import { AppAvailability } from "@ionic-native/app-availability/ngx";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  constructor(
+    private socialSharing: SocialSharing,
+    private appAvailability: AppAvailability,
+    private platform: Platform
+  ) { }
+  handleSocial() {
+    console.log("INNN");
+    var options = {
+      message: "share this", // not supported on some apps (Facebook, Instagram)
+      subject: "the subject", // fi. for email
+      files: ["", ""], // an array of filenames either locally or remotely
+      url: "https://www.website.com/foo/#bar?a=b",
+      chooserTitle: "Pick an app", // Android only, you can override the default share sheet title,
+      appPackageName: "com.whatsapp" // Android only, you can provide id of the App you want to share with
+    };
+    let app;
 
+    if (this.platform.is("ios")) {
+      app = "twitter://";
+    } else if (this.platform.is("android")) {
+      app = "com.whatsapp";
+    }
+    
+
+    this.appAvailability.check(app).then(data => {
+          console.log(app + ' is available');
+          this.socialSharing
+          .shareViaWhatsApp("test Message", "", "")
+          .then(data => {
+            console.log("Shared via SharePicker" + data);
+          })
+          .catch(err => {
+            console.log("Was not shared via SharePicker" + err);
+          });
+        })
+        .catch(err => {
+          console.log(app + ' is NOT available')
+        });
+     
+
+    // this.socialSharing
+    //   .canShareVia("com.whatsapp", "test Message", "teest subject")
+    //   .then(data => {
+    //     console.log("Shared via SharePicker" + data);
+    //     console.log("Please install whatsapp");
+    //   })
+    //   .catch(err => {
+    //     console.log("Was not shared via SharePicker" + err);
+    //   });
+  }
+
+  handlePushNotification() {
+    console.log("handlePushNotification");
+  }
 }
